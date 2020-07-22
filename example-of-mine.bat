5ff172b4de911decc95660255fcfef602fba7d57
Kaan <42345251+KaanOzkurt@users.noreply.github.com>
2020-07-22 23:20:52 +0300

    Update example-of-mine.bat

--- example-of-mine.bat
+++ example-of-mine.bat
@@ -1,35 +1,27 @@
 @echo off
 
+D:
 echo RESTORING FIRST MINUTE CHANGES
 cd "D:\Format\Files\"
 regedit.exe /S "initial.reg"
 
-echo INSTALLING VCREDIST
-cd "D:\Format\Files\vcredist"
-start /wait install_all.bat
-
 echo SETTING KEYBOARD LIGHTS
-cd "D:\Format\Files\"
-COPY "klavye-isigi.exe" "C:\Users\Kaan\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\klavye-isigi.exe"
+COPY "klavye-isigi.exe" "C:\Users\User\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\klavye-isigi.exe"
 start klavye-isigi.exe
 
 echo INSTALLING NVIDIA DRIVER
-cd "D:\Format\Files\451.67"
+cd "D:\Format\Files\451.67 Standart"
 start /wait setup.exe
-
-echo DELETE UNNEEDED PROGRAMS
-cd "D:\Programlar\Revo Uninstaller Pro"
-start /wait RevoUninstallerProPortable.exe
 
 echo SET UP FIREFOX
 cd "D:\Programlar\Firefox"
 start FirefoxPortable.exe
-start Firefox Register.exe
+start FirefoxRegister.exe
 pause
 
 echo PIN SHORTCUTS
 cd "D:\Format\Shortcuts"
-COPY "D:\Format\Shortcuts\*.lnk" "C:\Users\Kaan\AppData\Roaming\Microsoft\Windows\Start Menu\Programs"
+COPY "D:\Format\Shortcuts\*.lnk" "C:\Users\User\AppData\Roaming\OpenShell\Pinned"
 
 echo INSTALLING INTERNET DOWNLOAD MANAGER
 cd "D:\Format\Files\"
@@ -40,14 +32,15 @@ echo STARTING IDM
 C:
 cd "C:\Program Files (x86)\Internet Download Manager"
 start IDMan.exe
-D:
 
+D:
 echo INSTALLING WINRAR
 cd "D:\Format\Files\"
 start /wait winrar.exe
 
 echo SETTING SHAREX
 cd "D:\Programlar\ShareX\"
+COPY "ShareX.exe" "C:\Users\User\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ShareX.exe"
 start ShareX.exe
 
 echo SETTING MEM REDUCT
@@ -63,17 +56,12 @@ cd "D:\Format\Files\"
 start /wait dxwebsetup.exe /Q
 
 echo INSTALLING VS CODE
-start /wait "vscode.exe" /VERYSILENT /NORESTART
+start /wait vscode.exe
 echo RESTORING VS CODE SETTINGS
-COPY "settings.json" "C:\Users\Kaan\AppData\Roaming\Code\User\settings.json"
+XCOPY "settings.json" "C:\Users\User\AppData\Roaming\Code\User\settings.json"
 
 echo SETTING NVIDIA CONTROL PANEL LANGUAGE
-cd "D:\Format\Files\"
 regedit.exe /S "nvidia.reg"
-
-echo LAUNCHING DISCORD ONCE
-cd "D:\Programlar\Discord"
-start discord-portable.exe
 
 echo ACTIVATING WINDOWS
 cd "D:\Format\Files\"
@@ -100,32 +88,33 @@ echo MICROSOFT STORE SETTINGS
 ms-windows-store:
 pause
 
+echo INSTALL ADDITIONAL DRIVERS
+cd "D:\Format\Files\SDIO"
+start SDIO_auto.bat
+pause
+
 echo SET THUNDERBIRD AS DEFAULT
 cd "D:\Programlar\Thunderbird"
 start /wait ThunderbirdPortable.exe
 
-echo ADDING POWER PLAN
+echo SET UP POWER PLAN
+D:
 cd "D:\Format\Files\"
-powercfg /import "powerplan"
-echo CHOOSE THE NEW PLAN
+powercfg -import "D:\Format\Files\powerplan"
 powercfg.cpl
 pause
 
-echo INSTALLING Riva Tuner Statistics Server
-cd "D:\Format\Files\"
-start /wait rtss-setup.exe
+cd D:\Format\Files
+echo INSTALLING RivaTuner Statistics Server
+start /wait rtss.exe
 echo RESTORING RTSS SETTINGS
-COPY "Config" "C:\Program Files (x86)\RivaTuner Statistics Server\Profiles\Config"
-COPY "Global" "C:\Program Files (x86)\RivaTuner Statistics Server\Profiles\Global"
+XCOPY "Config" "C:\Program Files (x86)\RivaTuner Statistics Server\Profiles\Config"
+XCOPY "Global" "C:\Program Files (x86)\RivaTuner Statistics Server\Profiles\Global"
 echo STARTING RTSS
 C:
 cd "C:\Program Files (x86)\RivaTuner Statistics Server\"
 start rtss.exe
 D:
-
-echo INSTALL ADDITIONAL DRIVERS
-cd "D:\Format\Files\SDIO"
-start /wait SDIO_x64_R715.exe
 
 echo NVIDIA SETTINGS
 C:
@@ -133,18 +122,27 @@ cd "C:\Program Files\NVIDIA Corporation\Control Panel Client"
 start /wait nvcplui.exe
 
 echo RESTORE STEAM GAMES
+D:
 cd "D:\Programlar\Steam\"
 start steam.exe
 pause
 
 echo INSTALLING NEEDED PROGRAMS FOR UNITY ENGINE
-start /wait "dotnetsdk.exe"  /install /quiet /norestart
-start /wait "ndpdevpack.exe"  /install /quiet /norestart
+d:
+cd d:\format\files
+start /wait dotnetsdk.exe /install /quiet /norestart
+start /wait ndpdevpack.exe /install /quiet /norestart
 
+C:
 echo INSTALL C# EXTENSION FOR VS CODE
 cd "C:\Program Files\Microsoft VS Code"
-start /wait Code.exe
+start Code.exe
+pause
 
+D:
 echo SET UP UNITY HUB AND UNITY ENGINE
 cd "D:\Programlar\Unity Hub"
-start Unity Hub.exe
+start UnityHub.exe
+
+echo POST-INSTALL DONE! PLEASE RESTART
+pause
