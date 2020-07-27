109d416df9ef0e05e9d1ab17d0cf92e88e23a852
Kaan <42345251+KaanOzkurt@users.noreply.github.com>
2020-07-27 11:31:44 +0300

    Update example-of-mine.bat

--- example-of-mine.bat
+++ example-of-mine.bat
@@ -1,16 +1,51 @@
 @echo off
 
-D:
-echo RESTORING FIRST MINUTE CHANGES
 cd "D:\Format\Files\"
+
+echo RESTORING FIRST MINUTE CHANGES
 regedit.exe /S "initial.reg"
 
 echo SETTING KEYBOARD LIGHTS
-COPY "klavye-isigi.exe" "C:\Users\User\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\klavye-isigi.exe"
+XCOPY "klavye-isigi.exe" "C:\Users\User\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\klavye-isigi.exe"
 start klavye-isigi.exe
 
+echo INSTALLING INTERNET DOWNLOAD MANAGER
+start /wait idm.exe
+echo RESTORING IDM SETTINGS
+regedit.exe /S "idm.reg"
+
+echo INSTALLING WINRAR
+start /wait winrar.exe
+
+echo STOPPING WINDOWS SEARCH
+sc config "WSearch" start=disabled
+sc stop "WSearch"
+
+echo INSTALLING DIRECTX
+start /wait dxwebsetup.exe /Q
+
+echo INSTALLING VS CODE
+start /wait vscode.exe
+echo RESTORING VS CODE SETTINGS
+XCOPY "settings.json" "C:\Users\User\AppData\Roaming\Code\User\settings.json"
+
+echo ACTIVATING WINDOWS
+start /wait consoleact.exe /win=act
+
+echo INSTALLING NEEDED PROGRAMS FOR UNITY ENGINE
+start /wait dotnetsdk.exe /install /quiet /norestart
+start /wait ndpdevpack.exe /install /quiet /norestart
+
+echo INSTALLING HAMACHI
+msiexec /i hamachi.msi /quiet /norestart
+echo RESTORING HAMACHI SETTINGS
+sc stop "Hamachi2Svc"
+XCOPY "D:\Format\Files\LogMeIn Hamachi" "C:\Windows\ServiceProfiles\LocalService\AppData\Local\LogMeIn Hamachi"
+sc start "Hamachi2Svc"
+pause
+
 echo INSTALLING NVIDIA DRIVER
-cd "D:\Format\Files\451.67 Standart"
+cd "D:\Format\Files\451.67"
 start /wait setup.exe
 
 echo SET UP FIREFOX
@@ -21,53 +56,46 @@ pause
 
 echo PIN SHORTCUTS
 cd "D:\Format\Shortcuts"
-COPY "D:\Format\Shortcuts\*.lnk" "C:\Users\User\AppData\Roaming\OpenShell\Pinned"
-
-echo INSTALLING INTERNET DOWNLOAD MANAGER
-cd "D:\Format\Files\"
-start /wait idm.exe
-echo RESTORING IDM SETTINGS
-regedit.exe /S "idm.reg"
-echo STARTING IDM
-C:
-cd "C:\Program Files (x86)\Internet Download Manager"
-start IDMan.exe
-
-D:
-echo INSTALLING WINRAR
-cd "D:\Format\Files\"
-start /wait winrar.exe
+XCOPY "D:\Format\Shortcuts\*.lnk" "C:\Users\User\AppData\Roaming\OpenShell\Pinned"
 
 echo SETTING SHAREX
 cd "D:\Programlar\ShareX\"
-COPY "ShareX.exe" "C:\Users\User\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ShareX.exe"
+XCOPY "ShareX.lnk" "C:\Users\User\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ShareX.lnk"
 start ShareX.exe
 
 echo SETTING MEM REDUCT
 cd "D:\Programlar\Mem Reduct\"
 start memreduct.exe
 
-echo STOPPING WINDOWS SEARCH
-sc config "WSearch" start=disabled
-sc stop "WSearch"
+echo INSTALL ADDITIONAL DRIVERS
+cd "D:\Format\Files\SDIO"
+start SDIO_auto.bat
+pause
 
-echo INSTALLING DIRECTX
-cd "D:\Format\Files\"
-start /wait dxwebsetup.exe /Q
+echo SET THUNDERBIRD AS DEFAULT
+cd "D:\Programlar\Thunderbird"
+start /wait ThunderbirdPortable.exe
 
-echo INSTALLING VS CODE
-start /wait vscode.exe
-echo RESTORING VS CODE SETTINGS
-XCOPY "settings.json" "C:\Users\User\AppData\Roaming\Code\User\settings.json"
+echo RESTORE STEAM GAMES
+cd "D:\Programlar\Steam\"
+start steam.exe
+sleep 3
 
-echo SETTING NVIDIA CONTROL PANEL LANGUAGE
-regedit.exe /S "nvidia.reg"
+echo SET UP UNITY HUB AND UNITY ENGINE
+cd "D:\Programlar\Unity Hub"
+start UnityHub.exe
+sleep 3
 
-echo ACTIVATING WINDOWS
-cd "D:\Format\Files\"
-start /wait consoleact.exe /win=act
+C:
 
-echo MANUAL SETTINGS
+echo RESTORE OPEN SHELL SETTINGS
+cd "C:\Program Files\Open-Shell"
+start StartMenu.exe -settings
+pause
+
+echo STARTING IDM
+cd "C:\Program Files (x86)\Internet Download Manager"
+start IDMan.exe
 
 echo PERFORMANCE SETTINGS
 start /wait systempropertiesadvanced.exe
@@ -88,61 +116,19 @@ echo MICROSOFT STORE SETTINGS
 ms-windows-store:
 pause
 
-echo INSTALL ADDITIONAL DRIVERS
-cd "D:\Format\Files\SDIO"
-start SDIO_auto.bat
-pause
-
-echo SET THUNDERBIRD AS DEFAULT
-cd "D:\Programlar\Thunderbird"
-start /wait ThunderbirdPortable.exe
-
 echo SET UP POWER PLAN
-D:
-cd "D:\Format\Files\"
 powercfg -import "D:\Format\Files\powerplan"
 powercfg.cpl
 pause
 
-cd D:\Format\Files
-echo INSTALLING RivaTuner Statistics Server
-start /wait rtss.exe
-echo RESTORING RTSS SETTINGS
-XCOPY "Config" "C:\Program Files (x86)\RivaTuner Statistics Server\Profiles\Config"
-XCOPY "Global" "C:\Program Files (x86)\RivaTuner Statistics Server\Profiles\Global"
-echo STARTING RTSS
-C:
-cd "C:\Program Files (x86)\RivaTuner Statistics Server\"
-start rtss.exe
-D:
-
 echo NVIDIA SETTINGS
-C:
 cd "C:\Program Files\NVIDIA Corporation\Control Panel Client"
 start /wait nvcplui.exe
 
-echo RESTORE STEAM GAMES
-D:
-cd "D:\Programlar\Steam\"
-start steam.exe
-pause
-
-echo INSTALLING NEEDED PROGRAMS FOR UNITY ENGINE
-d:
-cd d:\format\files
-start /wait dotnetsdk.exe /install /quiet /norestart
-start /wait ndpdevpack.exe /install /quiet /norestart
-
-C:
 echo INSTALL C# EXTENSION FOR VS CODE
 cd "C:\Program Files\Microsoft VS Code"
 start Code.exe
 pause
-
-D:
-echo SET UP UNITY HUB AND UNITY ENGINE
-cd "D:\Programlar\Unity Hub"
-start UnityHub.exe
 
 echo POST-INSTALL DONE! PLEASE RESTART
 pause
